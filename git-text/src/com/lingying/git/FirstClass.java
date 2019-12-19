a2ef0ddaed6537c7ba4ced6cdcdbe1d5d3f0dbd1
liule <839400740@139.com>
2019-12-19 23:36:44 +0800

    吧本地工程添加到git

---
+++ git-text/src/com/lingying/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.lingying.git;
+
+public class FirstClass {
+    public static void main(String[] args){
+        System.out.println("hello.world");
+        System.out.println("hello.world");
+        System.out.println("hello.world");
+        System.out.println("hello.world");
+    }
+}
